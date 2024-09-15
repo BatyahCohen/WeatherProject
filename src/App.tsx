9d68e355475b3ff5b742cb222e9875b5ed5d5d61
Batya Cohen <batyah333@gmail.com>
2024-09-16 00:35:24 +0300

    API connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,70 @@ import "./bootstrap.css";
 import "./style.css";
 import { AreaDetailsComponent } from "./components/AreaDetailsComponent";
 import { AreaDetailsClass } from "./classes/AreaDetailsClass";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() 
 {
   const [areas, setAreas] = useState<Array<AreaDetailsClass>>
   ([
-    new AreaDetailsClass("אילת","",0,50,0,34.951925,29.557669),
-    new AreaDetailsClass("אלסקה","",0,-5,0,149.4937,64.2008),
-    new AreaDetailsClass("לונדון","",0,10,0,0.1278,51.5074),
-    new AreaDetailsClass("ניו יורק","",0,25,0,74.0060,40.7128),
+    new AreaDetailsClass("אילת","",500,500,500,34.951925,29.557669),
+    new AreaDetailsClass("אלסקה","",500,500,500,149.4937,64.2008),
+    new AreaDetailsClass("לונדון","",500,500,500,0.1278,51.5074),
+    new AreaDetailsClass("ניו יורק","",500,500,500,74.0060,40.7128),
   ]);
+
+  async function ApiConnect(lat:any,lon:any) {
+    try {
+      const apiKey = 'abc79528bbc77eafa1af5743ad2318e1'; 
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&units=metric&lang=he`;
+
+      let response = await fetch(url);
+     
+      let responseJson = await response.json();
+     
+      console.log(responseJson)
+      
+      return responseJson;
+    } 
+    catch (error) {
+      console.error(error);
+      alert("יש בעיה בטעינת המידע מהשרת, אנא נסה שוב מאוחר יותר")
+    }
+  }
+  
+  const updatedAreasByApi = async () => {
+   
+    const updatedAreas:Array<AreaDetailsClass> = await Promise.all(  
+      areas.map(async (area) => {
+        const responseJson = await ApiConnect(area.lat, area.lon);
+      
+        return new AreaDetailsClass(
+          area.name,
+          responseJson.weather[0].description,
+          responseJson.main.temp,
+          responseJson.main.feels_like,
+          responseJson.main.humidity,
+          area.lon,
+          area.lat
+        );
+      })
+    );
+   
+    setAreas(updatedAreas)
+  }
+ 
+ 
+  useEffect(() => {
+    updatedAreasByApi()    
+  }, []);
+
 
   return (
     <div className="App" >
       {areas?.map((area:AreaDetailsClass)=>(
         <div key={area.name}>
-          <AreaDetailsComponent area={area}></AreaDetailsComponent>
+        {area.temp!=500 && <AreaDetailsComponent area={area}></AreaDetailsComponent>}
         </div>
       ))}
     </div>
